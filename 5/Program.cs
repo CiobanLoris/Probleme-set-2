de2bf9aab97afaae1700ad0f21dcb06a5091da75
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:48:45 +0200

    Add files via upload

---
+++ 5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti lungimea secventei: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] secventa = new int[n];
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Introduceti elementul {i + 1}: ");
+            secventa[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int numarElementeEgaleCuPozitia = NumaraElementeEgaleCuPozitia(secventa);
+
+        Console.WriteLine($"In secventa, {numarElementeEgaleCuPozitia} elemente sunt egale cu pozitia la care apar.");
+
+        
+        Console.ReadKey();
+    }
+
+    static int NumaraElementeEgaleCuPozitia(int[] secventa)
+    {
+        int numarElementeEgaleCuPozitia = 0;
+
+        for (int i = 0; i < secventa.Length; i++)
+        {
+            if (secventa[i] == i)
+            {
+                numarElementeEgaleCuPozitia++;
+            }
+        }
+
+        return numarElementeEgaleCuPozitia;
+    }
+}
