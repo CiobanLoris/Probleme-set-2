a5d94e4deda596fe1fd68ea6762115f0451b8f8d
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:48:08 +0200

    Add files via upload

---
+++ 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti un numar n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int suma = CalculSuma(n);
+        long produs = CalculProdus(n);
+
+        Console.WriteLine($"Suma numerelor de la 1 la {n} este: {suma}");
+        Console.WriteLine($"Produsul numerelor de la 1 la {n} este: {produs}");
+
+        
+        Console.ReadKey();
+    }
+
+    static int CalculSuma(int n)
+    {
+        int suma = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            suma += i;
+        }
+
+        return suma;
+    }
+
+    static long CalculProdus(int n)
+    {
+        long produs = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            produs *= i;
+        }
+
+        return produs;
+    }
+}
