0d103d58cf18a0a73440df99330304563264db5e
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:49:37 +0200

    Add files via upload

---
+++ 8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti valoarea pentru n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int rezultat = Fibonacci(n);
+
+        Console.WriteLine($"Al {n}-lea numar din sirul lui Fibonacci este: {rezultat}");
+
+        
+        Console.ReadKey();
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            int a = 0;
+            int b = 1;
+            int rezultat = 0;
+
+            for (int i = 3; i <= n; i++)
+            {
+                rezultat = a + b;
+                a = b;
+                b = rezultat;
+            }
+
+            return rezultat;
+        }
+    }
+}
