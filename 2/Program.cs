e6fc8e6a296f65e6cee404e6978d3a4c091d2c9b
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:47:51 +0200

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti lungimea secventei: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] secventa = new int[n];
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Introduceti elementul {i + 1}: ");
+            secventa[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int numarNegative, numarZero, numarPozitive;
+        NumaraElemente(secventa, out numarNegative, out numarZero, out numarPozitive);
+
+        Console.WriteLine($"Dintre cele {n} numere, {numarNegative} sunt negative, {numarZero} sunt zero, iar {numarPozitive} sunt pozitive.");
+
+        
+        Console.ReadKey();
+    }
+
+    static void NumaraElemente(int[] secventa, out int numarNegative, out int numarZero, out int numarPozitive)
+    {
+        numarNegative = 0;
+        numarZero = 0;
+        numarPozitive = 0;
+
+        foreach (int numar in secventa)
+        {
+            if (numar < 0)
+            {
+                numarNegative++;
+            }
+            else if (numar == 0)
+            {
+                numarZero++;
+            }
+            else
+            {
+                numarPozitive++;
+            }
+        }
+    }
+}
