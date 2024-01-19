1415610b44ee787287d76e3c64b72a0e18829c95
CiobanLoris <146855824+CiobanLoris@users.noreply.github.com>
2024-01-19 16:50:48 +0200

    Add files via upload

---
+++ 11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti lungimea secventei: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        double[] secventa = new double[n];
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Introduceti elementul {i + 1}: ");
+            secventa[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        double sumaInverselor = CalculeazaSumaInverselor(secventa);
+
+        Console.WriteLine($"Suma inverselor este: {sumaInverselor}");
+
+        
+        Console.ReadKey();
+    }
+
+    static double CalculeazaSumaInverselor(double[] secventa)
+    {
+        double sumaInverselor = 0;
+
+        for (int i = 0; i < secventa.Length; i++)
+        {
+            sumaInverselor += 1 / secventa[i];
+        }
+
+        return sumaInverselor;
+    }
+}
